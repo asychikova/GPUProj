18e7d21d23d694e6f1acce0e169c3dcb52a7faf5
Anna Sychikova <102000143+asychikova@users.noreply.github.com>
2024-12-05 21:13:20 -0500

    graph2.py added

---
+++ pythonGraphs/graph2.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+
+processes = [2, 4, 6]
+rle_times = [109209475, 117075364, 157389475]
+lz77_times = [261392000000, 459934000000, 525532000000]
+huffman_times = [2524528107, 2246334908, 3130329339]
+
+plt.figure(figsize=(10, 6), facecolor='black') 
+ax = plt.gca()  
+ax.set_facecolor('black')  
+
+plt.plot(processes, rle_times, label='RLE', marker='o', color='cyan')
+plt.plot(processes, lz77_times, label='LZ77', marker='o', color='lime')
+plt.plot(processes, huffman_times, label='Huffman', marker='o', color='yellow')
+
+plt.xlabel('Number of Processes', color='white')
+plt.ylabel('Compression Time (ns)', color='white')
+plt.title('Compression Times for 2MB File with Different Algorithms', color='white')
+
+plt.yscale('log')  
+plt.xticks(processes, color='white')
+plt.yticks(color='white')
+plt.grid(True, linestyle='--', alpha=0.6, color='white')
+
+plt.legend(facecolor='black', edgecolor='white', labelcolor='white') 
+
+plt.show()
